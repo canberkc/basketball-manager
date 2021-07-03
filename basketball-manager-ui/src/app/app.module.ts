c656e633d9334758eeee3547c8212472b25438ba
canberkc <questionscb@gmail.com>
2021-07-03 13:51:12 +0200

    Re-organize and style the main nav
    Re-organize and style the main nav. Seperate the sive-nav component. Style main content.

--- basketball-manager-ui/src/app/app.module.ts
+++ basketball-manager-ui/src/app/app.module.ts
@@ -5,19 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './core/components/nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { SideNavComponent } from './core/components/side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavComponent
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
