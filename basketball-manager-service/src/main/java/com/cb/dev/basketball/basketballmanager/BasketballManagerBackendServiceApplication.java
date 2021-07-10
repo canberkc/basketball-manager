320fc2ab95745a6c06b0d98dc27dab0f4a8aa9c7
canberkc <questionscb@gmail.com>
2021-07-10 21:53:32 +0200

    Init backend service

---
+++ basketball-manager-service/src/main/java/com/cb/dev/basketball/basketballmanager/BasketballManagerBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cb.dev.basketball.basketballmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballManagerBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballManagerBackendServiceApplication.class, args);
+	}
+
+}
---
+++ basketball-manager-service/src/test/java/com/cb/dev/basketball/basketballmanager/BasketballManagerBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.dev.basketball.basketballmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballManagerBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
