8719ae0cfcd51da8873ad0637210ac1a21f06ab6
canberkc <questionscb@gmail.com>
2022-01-09 14:59:34 +0100

    Init actix-web and add gitignore for backend services

--- basketball-manager-service/src/main.rs
+++ basketball-manager-service/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello Basketball Manager!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, web, get, post};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(health_check)
+    })
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+}
+
+#[get("/greet")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello Basketball Manager!")
+}
+
+#[post("/health")]
+async fn health_check(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
